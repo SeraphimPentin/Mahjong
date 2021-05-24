41e2bf96093e4c0674a3941ce82f70abc1906bec
SeraphimPentin <70956362+SeraphimPentin@users.noreply.github.com>
2021-05-24 13:23:07 +0300

    Mahjong_1.1

--- app/src/main/java/com/example/mahjong/CardsAdapter.kt
+++ app/src/main/java/com/example/mahjong/CardsAdapter.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.concurrent.fixedRateTimer
 
-class CardsAdapter(val data: ArrayList<CardData>) : RecyclerView.Adapter<CardViewHolder>() {
+class CardsAdapter(private val data: ArrayList<CardData>) : RecyclerView.Adapter<CardViewHolder>() {
 
     private var holder1: CardViewHolder? = null
     private var holder2: CardViewHolder? = null
@@ -26,65 +28,80 @@ class CardsAdapter(val data: ArrayList<CardData>) : RecyclerView.Adapter<CardVie
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val realPosition = holder.adapterPosition
 
-        holder.cardView?.setOnClickListener {
-            holder2?.let { swap(it, position2!!) }
-            holder2 = holder1
-            position2 = position1
-            holder1 = holder
-            position1 = position
+        if (holder1 == null && holder2 == null) {
 
-            holder.cardView!!
-                .animate()
-                .rotationY(90f)
-                .setDuration(150)
-                .setListener(object : Animator.AnimatorListener {
-                    override fun onAnimationStart(animation: Animator?) {
-                    }
+            holder.cardView?.setOnClickListener {
+                holder2?.let { swap(it, position2!!) }
+                holder2 = holder1
+                position2 = position1
+                holder1 = holder
+                position1 = position
 
-                    override fun onAnimationEnd(animation: Animator?) {
-                        holder.cardView?.rotationY = -90f
-                        swap(holder, realPosition)
+                holder.cardView!!
+                    .animate()
+                    .rotationY(90f)
+                    .setDuration(150)
+                    .setListener(object : Animator.AnimatorListener {
+                        override fun onAnimationStart(animation: Animator?) {
+                        }
+
+                        override fun onAnimationEnd(animation: Animator?) {
+                            holder.cardView?.rotationY = -90f
+                            swap(holder, realPosition)
 
 
-                        holder.cardView!!
-                            .animate()
-                            .rotationY(0f)
-                            .setDuration(150)
-                            .setListener(object : Animator.AnimatorListener {
-                                override fun onAnimationStart(animation: Animator?) {
-                                }
-
-                                override fun onAnimationEnd(animation: Animator?) {
-                                    if (holder2 != null &&
-                                        holder1?.textView?.text?.isNotEmpty() == true &&
-                                        holder2?.textView?.text?.isNotEmpty() == true &&
-                                        holder1?.textView?.text?.toString()
-                                            .equals(holder2?.textView?.text?.toString())
-                                    ) {
-                                        holder1?.cardView?.animate()?.alpha(0f)
-                                            ?.setDuration(200)?.start()
-                                        holder2?.cardView?.animate()?.alpha(0f)
-                                            ?.setDuration(200)?.start()
-                                        holder1 = null
-                                        holder2 = null
+                            holder.cardView!!
+                                .animate()
+                                .rotationY(0f)
+                                .setDuration(150)
+                                .setListener(object : Animator.AnimatorListener {
+                                    override fun onAnimationStart(animation: Animator?) {
                                     }
-                                }
 
-                                override fun onAnimationCancel(animation: Animator?) {
-                                }
+                                    override fun onAnimationEnd(animation: Animator?) {
 
-                                override fun onAnimationRepeat(animation: Animator?) {
-                                }
-                            }).start()
-                    }
+                                        when {
+                                            holder1?.textView?.text.toString() ==
+                                                    holder2?.textView?.text.toString() -> {
+                                                holder1?.cardView?.animate()?.alpha(0f)
+                                                    ?.setDuration(200)?.start()
+                                                holder2?.cardView?.animate()?.alpha(0f)
+                                                    ?.setDuration(200)?.start()
+                                                holder1 = null
+                                                holder2 = null
 
-                    override fun onAnimationCancel(animation: Animator?) {
-                    }
+                                            }
+                                            holder1?.textView?.text.toString() !=
+                                                    holder2?.textView?.text.toString() -> {
 
-                    override fun onAnimationRepeat(animation: Animator?) {
-                    }
+                                                holder1?.cardView?.isInvisible
+                                                holder2?.cardView?.isInvisible
+                                            }
+                                            else -> {
+                                                holder1?.cardView?.animate()?.rotationY(180f)
+                                                holder2?.cardView?.animate()?.rotationY(180f)
+                                            }
+                                        }
 
-                }).start()
+
+                                    }
+
+                                    override fun onAnimationCancel(animation: Animator?) {
+                                    }
+
+                                    override fun onAnimationRepeat(animation: Animator?) {
+                                    }
+                                }).start()
+                        }
+
+                        override fun onAnimationCancel(animation: Animator?) {
+                        }
+
+                        override fun onAnimationRepeat(animation: Animator?) {
+                        }
+
+                    }).start()
+            }
         }
     }
 
--- app/src/main/java/com/example/mahjong/MainActivity.kt
+++ app/src/main/java/com/example/mahjong/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import com.example.mahjong.R.anim.card_show
 import com.example.mahjong.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
